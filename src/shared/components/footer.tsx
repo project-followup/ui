e1694be44107d6658fe615d11b1ad4c63f8efbbc
Łukasz Nowakowski <lukasz@lnow.pl>
2026-04-22 20:35:25 +0200

    Adjust styling of footer.
    Work on #9

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -1,10 +1,19 @@
 import styled from "@emotion/styled";
+import { themeTokens } from "@shared/types/themes";
 
 const Footer = styled.footer`
+  background-color: hsl(${themeTokens.backgroundColors.secondary});
+`;
+
+const FooterContent = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+  line-height: 3em;
 `;
 
 export default function FooterComponent() {
     return (<Footer>
-        <p>© 2024 Project FollowUp. All rights reserved.</p>
+        <FooterContent>© 2026 Project FollowUp. All rights reserved.</FooterContent>
     </Footer>);
 }
